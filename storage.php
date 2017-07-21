d3bf8589b160a697a3e4e5e5f9677284bebea2bc
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2017-07-22 01:37:50 +0530

    Update storage.php

--- storage.php
+++ storage.php
@@ -1,5 +1,5 @@
 <?php 
-$con = mysqli_connect("localhost","root","alphakimori","link");
+$con = mysqli_connect("localhost","root","","link");
 if(mysqli_connect_errno())
 {
 	echo "Failed to connect to MySQL: ".mysqli_connect_error();
